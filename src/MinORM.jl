5f5d08037c031ce98852cb738115d82d39a712e5
littlelib <littlelib@naver.com>
2023-07-10 20:41:08 +0900

    export fixed. again.

--- src/MinORM.jl
+++ src/MinORM.jl
@@ -1,11 +1,11 @@
 module MinORM
 
-using DataFrames, DotEnv, Pipe, SQLite, MySQL, LibPQ, Dates, StmtObject, concat
+using DataFrames, DotEnv, Pipe, SQLite, MySQL, LibPQ, Dates
 import Base
 
 
 include("./interface.jl")
 
-export DBManager, close!, reconnect!, prepare, execute, create, drop, insert, select, delete, update, Schema, Sql, P, String_, primary, autoincrement
+export DBManager, close!, reconnect!, prepare, execute, create, drop, insert, select, delete, update, Schema, Sql, P, String_, primary, autoincrement, StmtObject, concat
 
 end # module MinORM
