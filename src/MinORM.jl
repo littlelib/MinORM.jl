6ace70aceb0546bde8be376e14ab81070aa32da8
littlelib <littlelib@naver.com>
2023-07-10 20:45:21 +0900

    export fix

--- src/MinORM.jl
+++ src/MinORM.jl
@@ -6,6 +6,6 @@ import Base
 
 include("./interface.jl")
 
-export DBManager, close!, reconnect!, prepare, execute, create, drop, insert, select, delete, update, Schema, Sql, P, String_, primary, autoincrement, StmtObject, concat
+export DBManager, setup, close!, reconnect!, prepare, execute, create, drop, insert, select, delete, update, Schema, Sql, P, String_, primary, autoincrement, StmtObject, concat
 
 end # module MinORM
