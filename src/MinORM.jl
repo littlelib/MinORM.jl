ed1eb2f4bf1f8e4c6b11535a02010c3972a82a22
test <test@test.com>
2023-12-03 01:39:47 +0900

    update: export updated(connect)

--- src/MinORM.jl
+++ src/MinORM.jl
@@ -7,6 +7,6 @@ import Base
 include("./Interface.jl")
 include("devels.jl")
 
-export DBManager, @init, setup_prompt, close!, execute, execute_withdf, create, drop, insert, select, delete, update, Schema, statementbuilder, Sql, P, N, StmtObject, concat, String_, primary, autoincrement
+export DBManager, @init, setup_prompt, connect, close!, execute, execute_withdf, create, drop, insert, select, delete, update, Schema, statementbuilder, Sql, P, N, StmtObject, concat, String_, primary, autoincrement
 
 end # module MinORM
