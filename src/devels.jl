344c477d963688f5b4996f791ad25ae64ccc9745
test <test@test.com>
2023-12-05 00:11:53 +0900

    update: multiple inserts

--- src/devels.jl
+++ src/devels.jl
@@ -5,17 +5,20 @@ function insert(manager::DBManager, instances::Vector{T} where T<:Schema)
         error("Instances must be a vector of same schema")
     end
     schema_name=typeto_snakecase_name(instances|>eltype)
-    fields=fieldnames(instance|>eltype)|>collect
-    if autoincrement(instance|>eltype)==true && isa(getfield(instance, primary(instance)), Missing)
-        filter!(x->x!=primary(instance), fields)
+    fields=fieldnames(instances|>eltype)|>collect
+    if autoincrement(instances|>eltype)==true && isa(getfield(first(instances), primary(instances|>eltype)), Missing)
+        filter!(x->x!=primary(instances|>eltype), fields)
     end
     col_names=@pipe fields|>join(_, ", ")
     
-    stmt_values=@pipe fields|>
-    map(x->Sql("$(P(getfield(instance, x)))"), _)|>
-    collect|>
-    concat(_, ", ")
-    
-    stmt=Sql("insert into $(schema_name) ($(col_names)) values ($(N(stmt_values)));")
+    stmt_value_sets=map(instance->begin
+        @pipe fields|>
+        map(x->Sql("$(P(getfield(instance, x)))"), _)|>
+        concat(_, ", ")|>
+        Sql("($(N(_)))")
+        end, instances)|>
+    x->concat(x, ", ")
+        
+    stmt=Sql("insert into $(schema_name) ($(col_names)) values $(N(stmt_value_sets));")
     execute(manager, stmt)
 end
